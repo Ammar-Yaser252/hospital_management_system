b0f52801ab8f88ca287a8440b0d44068e3d0db44
Ammar Yasser <ammar.yasser83@outlook.com>
2026-08-20 06:14:10 +0300

    Add person class with working test

---
+++ src/models/person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, id_number, age, contact_info):
+        self.name = name
+        self.id_number = id_number
+        self.age = age
+        self.contact_info = contact_info
+
+        
---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+from src.models.person import Person
+
+first_person = Person("Ali", "101", 45, "01150650812")
+
+print (f"Name: {first_person.name}, ID: {first_person.id_number}, Age: {first_person.age}, Contact: {first_person.contact_info}")
+
