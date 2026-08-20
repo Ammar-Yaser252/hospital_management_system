9bcbdc39f283d84a907455bdca695549fea54acc
Ammar Yasser <ammar.yasser83@outlook.com>
2026-08-21 01:36:17 +0300

    Connect Patient to its own MedicalRecord via composition

--- src/models/patient.py
+++ src/models/patient.py
@@ -1,7 +1,8 @@
 from src.models.person import Person
+from src.models.medical_record import MedicalRecord
 
 class Patient(Person):
     def __init__(self, name, id_number, age, contact_info, blood_type):
         super().__init__(name, id_number, age, contact_info)
         self.blood_type = blood_type
-        self.record = None
+        self.record = MedicalRecord()
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,7 @@ from src.models.medical_record import MedicalRecord
 first_person = Person("Ali", "101", 45, "01150650812")  # person object
 first_doctor = Doctor("John", "1001", 50, "011556600882", "Oncology") # doctor object
 first_patient = Patient("Ahmed", "10001", 15, "012665588002", "A+") # patient object
-first_record = MedicalRecord()
+first_record = MedicalRecord() 
 
 
 print (f"Name: {first_person.name}, ID: {first_person.id_number}, Age: {first_person.age}, Contact: {first_person.contact_info}")
@@ -18,3 +18,7 @@ print (first_record.get_history())
 
 first_record.write_prescription("Take 3 panadol daily")
 print (first_record.get_history())
+
+
+first_patient.record.write_prescription("Take 1 brufen when needed")
+print (first_patient.record.get_history())
